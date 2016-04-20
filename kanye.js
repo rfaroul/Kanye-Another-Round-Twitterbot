9d65be2e1d967be2d04265a04163f74561e4354e
Rachelle Faroul <rachelle.faroul@gmail.com>
2016-04-19 20:21:26 -0400

    don't know why I'm getting error code 34 instead of 'already retweeted' error

--- kanye.js
+++ kanye.js
@@ -4,16 +4,47 @@ var troll = new Twitter(creds);
 
 console.log("bot is running");
 
+////////// BEGIN TEST //////////
+// troll.post('statuses/update', { status: "I'm posting a tweet!" }, function(error, tweet, response) {
+//   if(error) {
+//     console.log("There was a problem tweeting the message.", error);
+//   }
+// });
+////////// END TEST //////////
+
+////////// INITIATE THE BOT //////////
+function startBot(){
+	// var params = {screen_name: '@ggx568'};
+	// 	troll.get('statuses/user_timeline', params, function(error, tweets, response){
+	// 		if (!error) {
+	// 	  		console.log(tweets);
+	// 		}
+	// 		else{
+	// 			console.log(error);
+	// 		};
+	// 	});
+	troll.post('statuses/retweet/:id', { id: '241259202004267009' }, botInitiated);
+
+	function botInitiated(error, tweet, response){
+		if (error){
+			console.log("Bot could not be initiated. ", error);
+		}
+		else{
+			console.log("Bot initiated: 241259202004267009");
+		}
+	}
+};
+
+	
+startBot();
+
+
+
 //listen for tweets including the words "kanye" and "interview"
+
+
 //retweet the original tweet & include "copy"
 	//"copy" = "@kanyewest " + randomTip + " " + hebenQuote
 	//"copy" = "@kanyewest" + randomTip + linkToHebenArticle 
 
 //var TWITTER_SEARCH_PHRASE = '#technology OR #design';
-
-
-troll.post('statuses/update', { status: "I'm posting a tweet!" }, function(err, data, response) {
-  if(err) {
-    console.log("There was a problem tweeting the message.", err);
-  }
-});
