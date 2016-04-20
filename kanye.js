2deab9820c1d6e2729106dcbaaf683ef480a36cb
Rachelle Faroul <rachelle.faroul@gmail.com>
2016-04-19 20:51:32 -0400

    OR query and subsequent retweet working

--- kanye.js
+++ kanye.js
@@ -1,9 +1,10 @@
-var Twitter = require("twitter");
+var Twitter = require("twit");
 var creds = require("./auth.json");
 var troll = new Twitter(creds);
 
+//happens 1st
 console.log("bot is running");
-
+var twitter_search_phrase = '#kanye OR #interview';
 ////////// BEGIN TEST //////////
 // troll.post('statuses/update', { status: "I'm posting a tweet!" }, function(error, tweet, response) {
 //   if(error) {
@@ -14,28 +15,51 @@ console.log("bot is running");
 
 ////////// INITIATE THE BOT //////////
 function startBot(){
-	// var params = {screen_name: '@ggx568'};
-	// 	troll.get('statuses/user_timeline', params, function(error, tweets, response){
-	// 		if (!error) {
-	// 	  		console.log(tweets);
-	// 		}
-	// 		else{
-	// 			console.log(error);
-	// 		};
-	// 	});
 	troll.post('statuses/retweet/:id', { id: '241259202004267009' }, botInitiated);
 
-	function botInitiated(error, tweet, response){
+	function botInitiated(error, tweet, response){ //happens 3rd
 		if (error){
-			console.log("Bot could not be initiated. ", error); //getting a 'code 34' error instead of 'already retweeted' error. WHY?!
+			console.log("Bot could not be initiated. ", error); 
 		}
 		else{
 			console.log("Bot initiated: 241259202004267009");
 		}
 	}
+	retweetBot();
 };
+	function retweetBot(){
+		console.log("retweet something"); //happens 2nd before botInitiated()
+		var query = {
+			q: twitter_search_phrase,
+			result_type: "recent"
+		}
 
-	
+		troll.get("search/tweets", query, botGotLatestTweet);
+
+		function botGotLatestTweet(error, data, response){
+			if(error){
+				console.log("Bot could not find latest tweet " + error );
+			}
+			else{
+				var id = {
+					id: data.statuses[0].id_str
+				}
+				troll.post('statuses/retweet/:id', id, botRetweeted);
+
+				function botRetweeted(error, response){
+					if(error){
+						console.log('Bot could not retweet, ', error);
+					}
+					else{
+						console.log("Bot retweeted "+ id.id);
+					}
+				}
+			}
+		}
+	}
+
+
+
 startBot();
 
 
